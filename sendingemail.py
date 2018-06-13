881510a72c5ae40757385be70418ed247503e33f
Naman Saraswat <34354967+Namansaraswat7@users.noreply.github.com>
2018-06-13 16:35:42 +0530

    Update sendingemail.py

--- sendingemail.py
+++ sendingemail.py
@@ -2,8 +2,8 @@ import smtplib
  
 server = smtplib.SMTP('smtp.gmail.com',587)
 server.starttls()
-server.login("namansaraswat7@gmail.com", "namansar@1")
+server.login("yr email id", "yr password")
  
-msg = "hello naman ,this mais send to you  using python!"
-server.sendmail("namansaraswat7@gmail.com", "naman.15bec1090@abes.ac.in",msg)
+msg = "hello user ,this mail send to you  using python!"
+server.sendmail("sender email id", "receiver email id",msg)
 server.quit()
